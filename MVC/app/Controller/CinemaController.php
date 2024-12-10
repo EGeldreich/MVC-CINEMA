2c130cb88abc05f985efbb6391f57b5474f3fdcc
Emmanuel Geldreich <egeldreich@gmail.com>
2024-12-10 11:35:47 +0100

    Template

--- MVC/app/Controller/CinemaController.php
+++ MVC/app/Controller/CinemaController.php
@@ -7,39 +7,32 @@ use Model\Manager\PersonManager;
 use Model\Manager\CharacterManager;
 
 class cinemaController {
-
     // ----- LISTS -----
-
     // FILM LIST
     public function filmList() {
         $filmManager = new FilmManager();
         $films = $filmManager->getFilms();
         require "view/FilmList.php";
     }
-
     // ACTOR LIST
     public function actorList() {
         $actorManager = new PersonManager();
         $actors = $actorManager->getActors();
         require "view/ActorList.php";
     }
-    
     // DIRECTOR LIST
     public function directorList() {
         $directorManager = new PersonManager();
         $directors = $directorManager->getDirectors();
         require "view/DirectorList.php";
     }
-
     // CHARACTER LIST
     public function characterList() {
         $characterManager = new CharacterManager();
         $characters = $characterManager->getCharacters();
         require "view/CharacterList.php";
     }
-
     // ----- DETAILS -----
-
     // FILM DETAILS
     public function filmDetails() {
         $filmManager = new FilmManager();
@@ -47,7 +40,6 @@ class cinemaController {
         $castings = $filmManager->getCastings();
         require "view/FilmDetails.php";
     }
-
     // PERSON DETAILS
     public function personDetails() {
         $personManager = new PersonManager();
@@ -56,7 +48,6 @@ class cinemaController {
         $played = $personManager->getPlayed();
         require "view/PersonDetails.php";
     }
-
     // CHARACTER DETAILS
     public function characterDetails() {
         $characterManager = new CharacterManager();
--- MVC/app/View/Template.php
+++ MVC/app/View/Template.php
@@ -9,11 +9,27 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <title>The Movie Hub</title>
 </head>
 <body>
     <header>
+        <nav class="navbar flex-row">
+            <div class="navbar_left flex-row">
+                <a href="index.php">The Movie <span class="main-color">Hub</span></a>
+                <div class="navbar_left-main">
+                    <a href="FilmList.php">Movies</a>
+                    <a href="GenreList.php">Genres</a>
+                    <a href="ActorList.php">Actors</a>
+                    <a href="DirectorList.php">Directors</a>
+                </div>
+            </div>
+            <div class="navbar_right">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <a href="LoginForm.php">My Account</a>
+            </div>
+        </nav>
     </header>
     <main>
         <?= $content ?>
