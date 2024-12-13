9eb327b96ce9eac52bb2e3f24d430b2583b6feef
Emmanuel Geldreich <egeldreich@gmail.com>
2024-12-13 14:19:40 +0100

    CSS and JS styling of forms. Added Person roles to form

--- MVC/Public/js/script.js
+++ MVC/Public/js/script.js
@@ -3,19 +3,32 @@ const addGenreBtn = document.querySelector("#addGenre");
 const addFilmBtn = document.querySelector("#addFilm");
 const addPersonBtn = document.querySelector("#addPerson");
 const addCastingBtn = document.querySelector("#addCasting");
+const addCharacterBtn = document.querySelector("#addCharacter");
+
+const allBtns = document.querySelectorAll(".form-list_button");
 const forms = document.querySelectorAll(".forms");
 
-const formsClickHandler = (name) => {
+const formsClickHandler = (event) => {
+    // Get clicked btn
+    const clickedBtn = event.currentTarget;
+    //Get related form name from btn id
+    const formName = clickedBtn.id.replace("add", "").toLowerCase();
+
+    // Handle the active class of the buttons
+    allBtns.forEach((btn) =>
+        btn.classList.toggle("active", btn === clickedBtn)
+    );
+
+    // Handle the hidden class of the forms
     forms.forEach((form) => {
-        if (!form.classList.contains("hidden")) {
-            form.classList.add("hidden");
-        }
+        const isTargetForm = form.classList.contains(`${formName}-form`);
+        form.classList.toggle("hidden", !isTargetForm);
     });
-    const form = document.querySelector(`.${name}-form`);
-    form.classList.toggle("hidden");
 };
 
-addGenreBtn.addEventListener("click", () => formsClickHandler("genre"));
-addFilmBtn.addEventListener("click", () => formsClickHandler("film"));
-addPersonBtn.addEventListener("click", () => formsClickHandler("person"));
-addCastingBtn.addEventListener("click", () => formsClickHandler("casting"));
+// Add event listeners to all buttons
+[addGenreBtn, addFilmBtn, addPersonBtn, addCastingBtn, addCharacterBtn].forEach(
+    (btn) => {
+        btn.addEventListener("click", formsClickHandler);
+    }
+);
--- MVC/View/Content/formList.php
+++ MVC/View/Content/formList.php
@@ -2,125 +2,211 @@
 <h1 class="oswald">Add content</h1>
 
 <div class="form-list flex-row">
+    <button class="active form-list_button" id="addPerson">Add Person</button>
     <button class="form-list_button" id="addGenre">Add Genre</button>
     <button class="form-list_button" id="addFilm">Add Film</button>
-    <button class="form-list_button" id="addPerson">Add Person</button>
+    <button class="form-list_button" id="addCharacter">Add Character</button>
     <button class="form-list_button" id="addCasting">Add Casting</button>
 
 </div>
-<br>
-<br>
-<br>
 
-<div class="forms hidden genre-form">
-    <form method="post" action="index.php?action=addGenre">
-        <label for="gname">New Genre</label>
-        <input type="text" id="gname" name="gname" placeholder="Genre name" required>
+<!-- PERSON -->
+<div class="forms person-form raleway">
+    <form class="form flex-column" method="post" action="index.php?action=addPerson">
+        <div class="form-block flex-column">
 
-        <input type="submit" value="Submit" name="submit">
-    </form>
-</div>
-<br>
-<br>
-<br>
-
-<div class="forms hidden person-form">
-    <form method="post" action="index.php?action=addPerson">
-        <label for="firstname">First name</label>
-        <input type="text" id="firstname" name="firstname" placeholder="First name" required>
-        
-        <label for="lastname">Last name</label>
-        <input type="text" id="lastname" name="lastname" placeholder="Last name" required>
-
-        <label for="personGenre">Genre</label>
-        <select name="personGenre" id="personGenre" size="1" required>
-            <option value="Male">Male</option>
-            <option value="Female">Female</option>
-            <option value="Other">Other</option>
-        </select>
-
-        <label for="birthDate">Birthdate</label>
-        <input type="date" id="birthDate" name="birthDate" required>
-
-        <input type="submit" value="Submit" name="submit">
-    </form>
-</div>
-<br>
-<br>
-<br>
-
-<div class="forms hidden film-form">
-    <form method="post" action="index.php?action=addFilm">
-        <label for="title">Title</label>
-        <input type="text" id="title" name="title" placeholder="Title" required>
-        
-        <label for="releaseDate">Release Date</label>
-        <input type="date" id="releaseDate" name="releaseDate" required>
-
-        <label for="duration">Duration</label>
-        <input type="text" id="duration" name="duration" placeholder="Duration, in minutes" required>
-
-        <label for="director">Director</label>
-        <select name="director" id="director" size="1" required>
-            <?php foreach($directors as $director){ ?>
-                <option value='<?= $director['id_director'] ?>'><?= $director['director'] ?></option>
-            <?php } ?>
-        </select>
-        
-        <fieldset>
-            <legend>This film genres</legend>
-            <?php foreach($genres as $genre){ ?>
-                <div>
-                    <input type="checkbox" class="genre-checkbox" name="genre" value='<?= $genre['genre_name'] ?>'/>
-                    <label for='genre'><?= $genre['genre_name'] ?></label>
+            <div class="form-line flex-row">
+                <div class="input-and-label flex-column">
+                    <label for="firstname">First name</label>
+                    <input type="text" id="firstname" name="firstname" placeholder="First name" required>
                 </div>
-            <?php } ?>
-        </fieldset>
-        
-        <label for="rating">Rating</label>
-        <input type="text" id="rating" name="rating" placeholder="Rating, 0 through 10." required>
 
-        <label for="poster">Poster</label>
-        <input type="text" id="poster" name="poster" placeholder="Poster URL" required>
+                <div class="input-and-label flex-column">
+                    <label for="lastname">Last name</label>
+                    <input type="text" id="lastname" name="lastname" placeholder="Last name" required>
+                </div>
+            </div>
 
-        <label for="synopsis">Synopsis</label>
-        <textarea id="synopsis" name="synopsis" placeholder="Synopsis of the movie">
-        </textarea>
+            <div class="form-line flex-row">
+                <div class="input-and-label flex-column">
+                    <label for="personGenre">Genre</label>
+                    <select name="personGenre" id="personGenre" size="1" required>
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                        <option value="Other">Other</option>
+                    </select>
+                </div>
 
-        <input type="submit" value="Submit" name="submit">
+                <div class="input-and-label flex-column">
+                    <label for="birthDate">Birthdate</label>
+                    <input type="date" id="birthDate" name="birthDate" required>
+                </div>
+            </div>  
+
+            <div class="form-line flex-row">
+                <fieldset>
+                    <legend>Person role</legend>
+                    <div class="roles-field flex-row">
+                            <div class="roles-field_roles">
+                                <input type="checkbox" class="role-checkbox" name="role" value='actor'/>
+                                <label for='actor'>Actor</label>
+                            </div>
+                            <div class="roles-field_roles">
+                                <input type="checkbox" class="role-checkbox" name="role" value='director'/>
+                                <label for='director'>Director</label>
+                            </div>
+                    </div>
+                </fieldset>
+            </div>
+
+        </div>
+        <input type="submit" value="Add Person" name="submit">
     </form>
 </div>
 
-<br>
-<br>
-<br>
-<div class="forms hidden casting-form">
-    <form method="post" action="index.php?action=addCasting">
-        <label for="actor">Actor</label>
-        <select name="actor" id="actor" size="1" required>
-            <?php foreach($actors as $actor){ ?>
-                <option value='<?= $actor['id_actor'] ?>'><?= $actor['actor'] ?></option>
-            <?php } ?>
-        </select>
+<!-- GENRE -->
+<div class="forms hidden genre-form raleway">
+    <form class="form flex-column" method="post" action="index.php?action=addGenre">
+    <div class="form-block flex-column">   
+        <div class="input-and-label flex-column">
+                <label for="gname">New Genre</label>
+                <input type="text" id="gname" name="gname" placeholder="Genre name" required>
+            </div>
+        </div>
+        <input type="submit" value="Add Genre" name="submit">
+    </form>
+</div>
 
-        <p>is playing</p>
-        <label for="character">Character</label>
-        <select name="character" id="character" size="1" required>
-            <?php foreach($characters as $character){ ?>
-                <option value='<?= $character['name'] ?>'><?= $character['name'] ?></option>
-            <?php } ?>
-        </select>
+<!-- FILM -->
+<div class="forms hidden film-form raleway">
+    <form class="form flex-column" method="post" action="index.php?action=addFilm">
+        <div class="form-block-line flex-row">
+            <div class="form-block flex-column"> 
+    
+                <div class="form-line flex-row">
+                    <div class="input-and-label flex-column">
+                        <label for="title">Title</label>
+                        <input type="text" id="title" name="title" placeholder="Title" required>
+                    </div>
+                </div>
+    
+                <div class="form-line flex-row">
+                    <div class="input-and-label flex-column">
+                        <label for="director">Director</label>
+                        <select name="director" id="director" size="1" required>
+                            <?php foreach($directors as $director){ ?>
+                                <option value='<?= $director['id_director'] ?>'><?= $director['director'] ?></option>
+                            <?php } ?>
+                        </select>
+                    </div>
+                </div>
+    
+                <div class="form-line flex-row">
+                    <div class="input-and-label flex-column">
+                        <label for="releaseDate">Release Date</label>
+                        <input type="date" id="releaseDate" name="releaseDate" required>
+                    </div>
+                </div>
+    
+                <div class="form-line flex-row">
+                    <div class="input-and-label flex-column">
+                        <label for="duration">Duration</label>
+                        <input class="input_half-size"type="text" id="duration" name="duration" placeholder="Duration, in minutes" required>
+                    </div>
+    
+                    <div class="input-and-label flex-column">
+                        <label for="rating">Rating</label>
+                        <input class="input_half-size" type="text" id="rating" name="rating" placeholder="Rating, 0 through 10." required>
+                    </div>
+                </div>
+    
+                <div class="form-line flex-row">
+                    <div class="input-and-label flex-column">
+                        <label for="poster">Poster</label>
+                        <input type="text" id="poster" name="poster" placeholder="Poster URL" required>
+                    </div>
+                </div>
 
-        <p>in</p>
-        <label for="film">Film</label>
-        <select name="film" id="film" size="1" required>
-            <?php foreach($films as $film){ ?>
-                <option value='<?= $film['title'] ?>'><?= $film['title']?>, <?= substr($film['release_date'], 0, 4) ?></option>
-            <?php } ?>
-        </select>
+            </div>
+    
+            <div class="form-block flex-column"> 
+                <fieldset>
+                    <legend>This film genres</legend>
+                    <div class="genres-field flex-column">
+                        <?php foreach($genres as $genre){ ?>
+                            <div class="genres-field_genres">
+                                <input type="checkbox" class="genre-checkbox" name="genre" value='<?= $genre['genre_name'] ?>'/>
+                                <label for='genre'><?= $genre['genre_name'] ?></label>
+                            </div>
+                        <?php } ?>
+                    </div>
+                </fieldset>
+            </div>
+            
+            <div class="form-block flex-column"> 
+                <div class="input-and-label flex-column">
+                    <label for="synopsis">Synopsis</label>
+                    <textarea id="synopsis" name="synopsis" placeholder="Synopsis of the movie"></textarea>
+                </div>
+            </div>
+        </div>
+        <input type="submit" value="Add Film" name="submit">
+    </form>
+</div>
 
+<!-- CHARACTER -->
+<div class="forms hidden character-form raleway">
+    <form class="form flex-column" method="post" action="index.php?action=addCharacter">
+        <div class="form-block flex-column">   
+            <div class="input-and-label flex-column">
+                <label for="cname">New Character</label>
+                <input type="text" id="cname" name="cname" placeholder="Character name" required>
+            </div>
+        </div>  
+        <input type="submit" value="Add Character" name="submit">
+    </form>
+</div>
 
-        <input type="submit" value="Submit" name="submit">
+<!-- CASTING -->
+<div class="forms hidden casting-form raleway">
+    <form class="flex-column form" method="post" action="index.php?action=addCasting">
+        <div class="form-block flex-column">
+
+            <div class="form-line flex-row">
+                <div class="input-and-label flex-column">
+                    <label for="actor">Actor</label>
+                    <select name="actor" id="actor" size="1" required>
+                        <?php foreach($actors as $actor){ ?>
+                            <option value='<?= $actor['id_actor'] ?>'><?= $actor['actor'] ?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+
+                <p>is playing</p>
+
+                <div class="input-and-label flex-column">
+                    <label for="character">Character</label>
+                    <select name="character" id="character" size="1" required>
+                        <?php foreach($characters as $character){ ?>
+                            <option value='<?= $character['name'] ?>'><?= $character['name'] ?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+
+                <p>in</p>
+
+                <div class="input-and-label flex-column">
+                    <label for="film">Film</label>
+                    <select name="film" id="film" size="1" required>
+                        <?php foreach($films as $film){ ?>
+                            <option value='<?= $film['title'] ?>'><?= $film['title']?>, <?= substr($film['release_date'], 0, 4) ?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+            </div>
+
+        </div>
+        <input type="submit" value="Add Casting" name="submit">
     </form>
 </div>
 
