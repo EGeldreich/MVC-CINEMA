5c3bc817588347534965650e233de9ea24693d6a
Emmanuel Geldreich <egeldreich@gmail.com>
2024-12-18 16:17:35 +0100

    Update Connect.php comments

--- MVC/Model/Connect.php
+++ MVC/Model/Connect.php
@@ -2,11 +2,11 @@
 namespace Model;
 
 abstract class Connect {
-    const HOST = "localhost"; // Port par défaut de MAMP
+    const HOST = "localhost"; // to use with HeidiSQL
     const DB = "cinema_emmanuel";
     const USER = "root";
     const PASS = "";
-    // const HOST = "localhost:8889"; // Port par défaut de MAMP
+    // const HOST = "localhost:8889"; // to use with MAMP
     // const DB = "cinemadb";
     // const USER = "root";
     // const PASS = "root";
@@ -20,7 +20,7 @@ abstract class Connect {
             );
             return $pdo;
         } catch (\PDOException $ex) {
-            // Lever une exception en cas d'erreur
+            // catch exception error
             die("Erreur de connexion à la base de données : " . $ex->getMessage());
         }
     }
